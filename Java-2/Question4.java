3689022d7cb044d4b5b0138bf07a89c9a80a7998
shashankp10-TTN <shashank.pandey@tothenew.com>
2025-02-05 10:04:36 +0530

    docs: add Question 5 (singleton exercise question)

---
+++ Java-2/Question4.java
@@ -0,0 +1,5 @@
+/*
+    Write a program to show application of Factory Design Pattern.
+ */
+public class Question4 {
+}
---
+++ Java-2/Question5.java
@@ -0,0 +1,23 @@
+
+class Singleton {
+    private static Singleton sqlInstance;
+    private Singleton(){
+        System.out.println("SQL is connected!!");
+    }
+
+    public static Singleton getInstance() {
+        if(sqlInstance==null) {
+            sqlInstance = new Singleton();
+        }
+        return sqlInstance;
+    }
+    public void dummyMethod(){
+        System.out.println("Dummy method called using singleton instance!!");
+    }
+}
+public class Question5 {
+    public static void main(String[] args) {
+        Singleton instance = Singleton.getInstance();
+        instance.dummyMethod();
+    }
+}
