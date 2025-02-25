4d9eab59fdbd9f69e1d34f7c390b685b1dc5c231
shashankp10-TTN <shashank.pandey@tothenew.com>
2025-02-25 10:07:20 +0530

    test: add test for EmailService

---
+++ Junit/src/main/java/com/JUnit/JunitLearningApplication.java
@@ -0,0 +1,13 @@
+package com.JUnit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitLearningApplication.class, args);
+	}
+
+}
---
+++ Junit/src/main/java/com/JUnit/service/TestService.java
@@ -0,0 +1,7 @@
+package com.JUnit.service;
+
+public class TestService {
+    public int test() {
+        return 1;
+    }
+}
